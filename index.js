89d4e83066c9209f695923ef7e95a742f1af6447
Mauricio <mauriciomeza95@hotmail.com>
2022-02-03 09:33:23 -0600

    Start

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb+srv://mauriciomeza:Taquito2021!@cluster0.rgjhw.mongodb.net/test';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
@@ -21,3 +24,18 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+  Recipe.create({
+    name: "Burguer",
+    level: "Easy Peasy",
+    ingredients: ['bread', 'lettuce', 'meat', 'salt', 'pepper', 'tomatoes', 'ketchup'],
+    dishType: 'main_course',
+    duration: 35,
+    creator: 'Ronald McDonald'
+  })
+  .then((r) => console.log(r))
+  .catch((e) => console.log(e))
+
+  Recipe.insertMany(data).then(res =>console.log(res)).catch(e =>console.log(e))
